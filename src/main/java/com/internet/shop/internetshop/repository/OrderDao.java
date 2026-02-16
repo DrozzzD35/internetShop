97675bfbab3e2e3fde4b3970d26b51530337e87a
DrozzzD35 <kapustin3595@yandex.ru>
2026-02-16 15:38:57 +0300

    feat: add OrderRepository

---
+++ src/main/java/com/internet/shop/internetshop/repository/OrderDao.java
@@ -0,0 +1,20 @@
+package com.internet.shop.internetshop.repository;
+
+import com.internet.shop.internetshop.model.Order;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderDao {
+
+    Optional<Order> findOrderById(long id);
+
+    List<Order> findAllOrders();
+
+    Order createOrder(Order order);
+
+    void removeOrder(Order order);
+
+    Optional<Order> updateOrder(Order order);
+
+}
--- src/main/java/com/internet/shop/internetshop/repository/OrderRepository.java
+++ src/main/java/com/internet/shop/internetshop/repository/OrderRepository.java
@@ -1,4 +1,52 @@
 package com.internet.shop.internetshop.repository;
 
-public class OrderRepository {
+import com.internet.shop.internetshop.model.Order;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class OrderRepository implements OrderDao {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public Optional<Order> findOrderById(long id) {
+        Order order = manager.find(Order.class, id);
+        return Optional.ofNullable(order);
+    }
+
+    @Override
+    public List<Order> findAllOrders() {
+        return manager.createQuery("SELECT o FROM Order o", Order.class)
+                .getResultList();
+    }
+
+    @Override
+    public Order createOrder(Order order) {
+        if (order.getId() != 0) {
+            throw new RuntimeException("Ошибка при создании заказа");
+        }
+        manager.persist(order);
+        return order;
+    }
+
+    @Override
+    public void removeOrder(Order order) {
+        Optional<Order> optionalOrder = findOrderById(order.getId());
+        optionalOrder.ifPresent(manager::remove);
+    }
+
+    @Override
+    public Optional<Order> updateOrder(Order order) {
+        if (order.getId() == 0) {
+            throw new RuntimeException("Ошибка при обновлении заказа");
+        }
+        Order updatedOrder = manager.merge(order);
+        return Optional.of(updatedOrder);
+    }
 }
